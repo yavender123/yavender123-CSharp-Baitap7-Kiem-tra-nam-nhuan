56ae135a3144454ae050980f23294e21767ac0e6
Duc Anh <yavenderv@gmail.com>
2026-02-23 02:58:45 +0700

    First commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Bai7
+{
+    static void Main(string[] args)
+    {
+        //Opening
+        Console.WriteLine($"Hello!");
+
+        //Nhap so nam
+        Console.WriteLine($"Nam: ");
+        int nam  = int.Parse(Console.ReadLine()!);
+
+        //Kiem tra nam nhuan va in ra destop
+        if ((nam % 400 == 0) || (nam % 4 == 0 && nam % 100 != 0))
+        {
+            Console.WriteLine($"{nam} la nam nhuan");
+        }
+        else
+        {
+            Console.WriteLine($"{nam} khong phai nam nhuan");
+        }
+    }
+}
